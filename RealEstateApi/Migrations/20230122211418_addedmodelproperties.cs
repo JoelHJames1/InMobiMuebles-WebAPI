9233c52fb7c810aa0adbc855f86f071ffeeaeb63
Joel Hernandez <Joel.Hernandez.James@gmail.com>
2023-07-04 17:03:11 -0400

    Add project files.

---
+++ RealEstateApi/Migrations/20230122211418_addedmodelproperties.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateApi.Migrations
+{
+    public partial class addedmodelproperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileImageUrl",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NumOfBathrooms",
+                table: "Properties",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NumOfBedRooms",
+                table: "Properties",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PropertySize",
+                table: "Properties",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "PropertyPhoto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PropertyId = table.Column<int>(type: "int", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropertyPhoto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PropertyPhoto_Properties_PropertyId",
+                        column: x => x.PropertyId,
+                        principalTable: "Properties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PropertyPhoto_PropertyId",
+                table: "PropertyPhoto",
+                column: "PropertyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PropertyPhoto");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileImageUrl",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "NumOfBathrooms",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "NumOfBedRooms",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "PropertySize",
+                table: "Properties");
+        }
+    }
+}
---
+++ RealEstateApi/Models/PropertyPhoto.cs
@@ -0,0 +1,9 @@
+﻿namespace InMobiMuebles_WebAPI.Models
+{
+    public class PropertyPhoto
+    {
+        public int Id { get; set; }
+        public int PropertyId { get; set; }
+        public string ImageUrl { get; set; }    
+    }
+}
